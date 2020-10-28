e432d8ea1ea105fffa64b9af57d0e139a5e67905
Anshul Yadav <55681256+anshulforyou@users.noreply.github.com>
2020-10-28 21:46:27 +0530

    Update Q1_part1.c

--- Q1/Q1_part1.c
+++ Q1/Q1_part1.c
@@ -1,2 +1,33 @@
-/* Name: Your Name
-   Roll_Number: Your Roll_Number */
+/* Name: Anshul Yadav
+   Roll_Number: 2019023 */
+#include<stdio.h>
+#include<string.h>
+#include<unistd.h>
+#include <sys/wait.h> 
+#include <sys/types.h> 
+#include <sys/stat.h>
+
+void main(){
+	int x = 10;
+	pid_t pid;
+	pid = fork();
+	if(pid<0){
+		fprintf(stderr, "%s\n", "Fork Failed");
+	}
+	else if (pid ==0){
+		printf("%s\n", "Child process:");
+		printf("%s %d\n", "Initial value:", x);
+		for(x;x>-90;){
+			x--;
+		}
+		printf("%s %d\n", "Final value:", x);
+	}else{
+		// wait(NULL);
+		printf("%s\n", "Parent process");
+		printf("%s %d\n", "Initial value:", x);
+		for(x;x<100;){
+			x++;
+		}
+		printf("%s %d\n", "Final value:", x);
+	}
+}
